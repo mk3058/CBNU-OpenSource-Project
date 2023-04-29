bfeecc75d59f624bdb7dce606fd051411566c9c6
mk3058 <kmk3058@icloud.com>
2023-04-29 11:08:14 +0900

    feat: add AuthController

--- src/main/java/com/minkyu/myproject/MyprojectApplication.java
+++ src/main/java/com/minkyu/myproject/MyprojectApplication.java
@@ -1,10 +1,13 @@
 package com.minkyu.myproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 @ConfigurationPropertiesScan
 public class MyprojectApplication {
 
