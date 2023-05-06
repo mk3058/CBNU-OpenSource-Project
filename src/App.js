ef304b0ce34cb03d9ded2a981adf61ab9a903f3e
mk3058 <kmk3058@icloud.com>
2023-05-06 15:32:08 +0900

    feat: add location drop down menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import locationData from './locations.json';
 
 const SERVICE_KEY = "#### API SERVICE KEY ####";
 const DATA_TYPE = "JSON";
@@ -11,11 +12,9 @@ function getPreviousDate() {
   return currentDate.toISOString().slice(0, 10).replace(/-/g, "");
 }
 
-async function fetchWeatherData() {
+async function fetchWeatherData(NX, NY) {
   const BASE_DATE = getPreviousDate();
   const BASE_TIME = "2300";
-  const NX = "55";
-  const NY = "128";
 
   const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?ServiceKey=${SERVICE_KEY}&pageNo=1&numOfRows=290&dataType=${DATA_TYPE}&base_date=${BASE_DATE}&base_time=${BASE_TIME}&nx=${NX}&ny=${NY}`;
 
@@ -59,24 +58,107 @@ function WeatherChart({ data }) {
   );
 }
 
+function getNxNy(city, district, neighborhood) {
+  const location = locationData.find(
+    (loc) =>
+      loc.city === city &&
+      loc.district === district &&
+      loc.neighborhood === neighborhood
+  );
+
+  if (location) {
+    return { nx: location.nx, ny: location.ny };
+  } else {
+    throw new Error("Location not found");
+  }
+}
+
+const uniqueCities = [...new Set(locationData.map((loc) => loc.city))];
+const uniqueDistricts = (city) => [
+  ...new Set(
+    locationData
+      .filter((loc) => loc.city === city)
+      .map((loc) => loc.district)
+  ),
+];
+const uniqueNeighborhoods = (city, district) => [
+  ...new Set(
+    locationData
+      .filter((loc) => loc.city === city && loc.district === district)
+      .map((loc) => loc.neighborhood)
+  ),
+];
+
 function App() {
   const [chartData, setChartData] = useState([]);
+  const [city, setCity] = useState("");
+  const [district, setDistrict] = useState("");
+  const [neighborhood, setNeighborhood] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      const weatherData = await fetchWeatherData();
-      const data = extractChartData(weatherData);
-      setChartData(data);
-    }
-    fetchData();
-  }, []);
+  async function fetchData(nx, ny) {
+    const weatherData = await fetchWeatherData(nx, ny);
+    const data = extractChartData(weatherData);
+    setChartData(data);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { nx, ny } = getNxNy(city, district, neighborhood);
+    fetchData(nx, ny);
+  }  
 
   return (
     <div className="App">
       <h1>Weather Visualization</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          시/도:
+          <select value={city} onChange={(e) => setCity(e.target.value)}>
+            <option value="">Select City</option>
+            {uniqueCities.map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label>
+          시/군/구:
+          <select
+            value={district}
+            onChange={(e) => setDistrict(e.target.value)}
+            disabled={!city}
+          >
+            <option value="">Select District</option>
+            {uniqueDistricts(city).map((district) => (
+              <option key={district} value={district}>
+                {district}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label>
+          읍/면/동:
+          <select
+            value={neighborhood}
+            onChange={(e) => setNeighborhood(e.target.value)}
+            disabled={!district}
+          >
+            <option value="">Select Neighborhood</option>
+            {uniqueNeighborhoods(city, district).map((neighborhood) => (
+              <option key={neighborhood} value={neighborhood}>
+                {neighborhood}
+              </option>
+            ))}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       <WeatherChart data={chartData} />
     </div>
   );
+  
 }
+
 
 export default App;
